157a0a1a61e115b53c7ad1996cce4e91e615f150
chiyu <chiyu_lin@asus.com>
2024-12-07 15:35:10 +0800

    use_middleware

--- src/login/Login.js
+++ src/login/Login.js
@@ -22,7 +22,7 @@ const Login = ({ onSubmit }) => {
     try {
       // 使用 axios 發送 POST 請求
       const response = await axios.post(
-        'https://ocbc-ticket.vercel.app/api/proxy/clientportal2/Auth/Login',
+        '/api/clientportal2/Auth/Login',
         payload,
         {
           headers: {
