4d4b848e663ed522fa2c0749965921e7604dadc6
chiyu <chiyu_lin@asus.com>
2024-12-07 13:58:09 +0800

    fix url

--- src/book/Book.js
+++ src/book/Book.js
@@ -98,7 +98,7 @@ const Book = ({
         };
 
         const response = await axios.post(
-          'http://localhost:3000/api/proxy/clientportal2/FacilityBookings/WizardSteps/ChooseBookingRuleStep/Next',
+          'https://ocbc-ticket.vercel.app/api/proxy/clientportal2/FacilityBookings/WizardSteps/ChooseBookingRuleStep/Next',
           payload,
           {
             headers: {
